dc2a634ebfea0b886e190ca6c0303c152baee721
TheEgyptianlion <laithalmasri3000@gmail.com>
2020-11-28 14:56:27 -0500

    Added Email Splicer Example

---
+++ Email Splicer.py
@@ -0,0 +1,10 @@
+#Given an Email return username & directory
+#sample: username@directory.com
+
+def splice(email):
+    username = email[:email.index("@")]
+    directory = email[email.index("@")+1:]
+    return (username,directory)
+email = 'test@sample.com'
+username,directory = splice(email)
+print("Username: {user} Directory: {dir}".format(user=username,dir=directory))
